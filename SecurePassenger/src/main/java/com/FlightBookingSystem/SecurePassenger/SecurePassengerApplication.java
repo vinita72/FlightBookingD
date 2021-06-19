130649448f41a28be44a11239d97bdd77a9eb415
vinita72 <imvinitarajoria@gmail.com>
2021-06-19 16:49:26 +0530

    commit secure R

--- SecurePassenger/src/main/java/com/FlightBookingSystem/SecurePassenger/SecurePassengerApplication.java
+++ SecurePassenger/src/main/java/com/FlightBookingSystem/SecurePassenger/SecurePassengerApplication.java
@@ -1,49 +1,68 @@
 package com.FlightBookingSystem.SecurePassenger;
 
-import java.util.List;
-import com.FlightBookingSystem.SecurePassenger.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.FlightBookingSystem.SecurePassenger.repository.UserRepo;
 
 
-@SpringBootApplication
-public class SecurePassengerApplication implements CommandLineRunner{
-	@Autowired
-	private UserRepo repository;
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class, 
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+@CrossOrigin(origins ="*")
+public class SecurePassengerApplication{
+	
+	private static final Logger Log =LoggerFactory.getLogger(SecurePassengerApplication.class);
+	
 	
 	public static void main(String[] args) {
+		
+		Log.info("Application Started");
 		SpringApplication.run(SecurePassengerApplication.class, args);
 		
 	}
-		public void run(String... args) throws Exception {
-			// TODO Auto-generated method stub
-			
-			
-			User s1 = new User(6, "ramra","$2a$04$DVZXitHA2Q4ehyUO2SKqGOIxSNeP/GxVFWtEuRktxLw32cKnWMnoK");
-			User s2 = new User(5,"asklily", "$2a$04$prhYvk2noo5tkPcrUn9XqO4mheUkBSVq0RgR/ZdeN/VuyRPmlf4xK");
-
-			
-			repository.save(s1);
-			repository.save(s2);
-			
-			
-			System.out.println("***");
-			
-			
-			List<User> users = repository.findAll();
-			for (User s : users) {
-				System.out.println(s.toString());
-			}
-		
-	}
-
+	
+	
+	
 	
 	
 }
+	
+	/*
+	 * public void run(String... args) throws Exception { // TODO Auto-generated
+	 * method stub
+	 * 
+	 * 
+	 * User s1 = new User("7",
+	 * "ravi","$2a$04$qfQfLnh79PUog6nwmO7noOY1azw4tPYGKvKCR8NXYyHvJD2fKhPt6",
+	 * "ravi@gmail.com"); User s2 = new User("9", "Vachi",
+	 * "$2a$04$3jankoGXp6uTBk5ZGrHOYO6AGo.WX31.s6kXeUWmPwN7vqB7AlpLy",
+	 * "vachi@gmail.com");
+	 * 
+	 * 
+	 * repository.save(s1); repository.save(s2);
+	 * 
+	 * 
+	 * System.out.println("***");
+	 * 
+	 * 
+	 * List<User> users = repository.findAll(); for (User s : users) {
+	 * System.out.println(s.toString()); }
+	 * 
+	 * }
+	 */
+
+	
+	
+
 
 
 
--- SecurePassenger/src/main/java/com/FlightBookingSystem/SecurePassenger/model/Users.java
+++ SecurePassenger/src/main/java/com/FlightBookingSystem/SecurePassenger/model/Users.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,30 +30,34 @@ import lombok.ToString;
 @Setter
 @ToString
 @Document(collection ="users")
-public class User{
+public class Users{
 
 	@Id
-	private int id;
-	private String username;
-	private String password;
+	private  String id;
+	private  String username;
+	private  String password;
+	private  String emailid;
+	private String role;
 	
 	
-	
-	
-	public User() {
+	public Users() {
 	}
 	
-	public User(int id, String username, String password) {
+	public Users(String id, String username, String password, String emailid, String role) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
+		this.emailid = emailid;
+		this.role = role;
 	}
 	
 	
-	public int getId() {
+	
+
+	public  String getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public String getUsername() {
@@ -61,12 +66,29 @@ public class User{
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	public String getPassword() {
+	public  String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public  String getEmailid() {
+		return emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 	
 
 	
